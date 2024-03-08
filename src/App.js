f5ceab1e9ab21a85b04e752f19458e103113744c
GabrielStanca <stanca.gabriel.alx@gmail.com>
2024-03-08 18:46:00 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {GetData} from "./components/getData";
+
+function App() {
+  return (
+    <div className="App">
+      <GetData />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/getData.jsx
@@ -0,0 +1,10 @@
+
+export const GetData = () => {
+    const movieTitle = 'Jumanji (1995)';
+    fetch(`http://127.0.0.1:8000/recommendations/${encodeURIComponent(movieTitle)}`)
+        .then((response) => response.json())
+        .then((data) => {
+            // Update your component's state or handle the recommendations data as needed
+            console.log(data);
+        });
+}
