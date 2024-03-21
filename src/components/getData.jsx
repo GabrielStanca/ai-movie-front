f8b6af7d9306564712ba24b3c8135ee6a8a3cdf6
GabrielStanca <stanca.gabriel.alx@gmail.com>
2024-03-21 21:50:04 +0200

    push front cu input si exemplu

--- src/components/getData.jsx
+++ src/components/getData.jsx
@@ -1,13 +1,27 @@
-
+import './style.css'
+import {useState} from "react";
 export const GetData = () => {
-    const movieTitle = 'Jumanji (1995)';
-    fetch(`http://127.0.0.1:8000/recommendations/${encodeURIComponent(movieTitle)}`)
-        .then((response) => response.json())
-        .then((data) => {
-            // Update your component's state or handle the recommendations data as needed
-            console.log(data);
-        });
+    const [text,setText] = useState('I\'m looking for a Jumanji (1995) and a romance  movie.')
+    const onSubmit = (e) => {
+        e.preventDefault()
+        fetch(`http://127.0.0.1:8000/recommendations/${encodeURIComponent(text)}`)
+            .then((response) => response.json())
+            .then((data) => {
+                // Update your component's state or handle the recommendations data as needed
+                console.log(data);
+            });
+    }
+
+
     return(
-        <h1>!!!Verifica consola coios!!!</h1>
+        <div className={'ask-ai-container'}>
+            <form onSubmit={onSubmit}>
+                <label htmlFor={'input'} >Write here a genres of a movie or directly a movie and we will give you a recomandation</label>
+                <input value={text} onChange={(e)=>{
+                    setText(e.target.value)
+                }} type={'text'} name={'input'} id={'input'} placeholder={"Type here"} />
+                <button type={'submit'} disabled={!text}>Submit</button>
+            </form>
+        </div>
     )
 }
